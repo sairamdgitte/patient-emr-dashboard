58c87b80bf91b418fce3422603a793758a966269
Mondo <97865274+Mondokad@users.noreply.github.com>
2026-08-13 14:49:43 +1000

    Update data.js
    change phone

--- src/components/data.js
+++ src/components/data.js
@@ -194,5 +194,5 @@ export const enrichSinglePatient = (rawPatient, index) => {
 export const WH_IC = "https://hooks.au.webexconnect.io/events/OWA9LRB1FL";
 export const WH_AI = "https://hooks.au.webexconnect.io/events/LHLVF8UJTQ";
 export const HF_SPACE = "https://sairam17-patient-summary-api.hf.space/gradio_api/call/predict";
-export const PAT_PHONE = "61422251212";
+export const PAT_PHONE = "61430246929";
 export const HOST_PHONE = "61414005070";
