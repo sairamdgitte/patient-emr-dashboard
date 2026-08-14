7df90595b2a846e03773b43dbf2bb250b271eb98
Mondo <97865274+Mondokad@users.noreply.github.com>
2026-08-14 10:35:28 +1000

    Update data.js
    updated to work on emails

--- src/components/data.js
+++ src/components/data.js
@@ -191,8 +191,8 @@ export const enrichSinglePatient = (rawPatient, index) => {
 };
 
 // Webhook URLs
-export const WH_IC = "https://hooks.au.webexconnect.io/events/OWA9LRB1FL";
-export const WH_AI = "https://hooks.au.webexconnect.io/events/LHLVF8UJTQ";
+export const WH_IC = "https://hooks.au.webexconnect.io/events/DRZFTVMQXP";
+export const WH_AI = "https://hooks.au.webexconnect.io/events/OWA9LRB1FL";
 export const HF_SPACE = "https://sairam17-patient-summary-api.hf.space/gradio_api/call/predict";
-export const PAT_PHONE = "61430246929";
-export const HOST_PHONE = "61414005070";
+export const PAT_PHONE = "mondo.k@rmit.edu.au";
+export const HOST_PHONE = "marcus.lim3@rmit.edu.au";
